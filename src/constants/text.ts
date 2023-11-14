7ad2f53c994f9eafb5deea5854152b92220b88e8
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-11-14 20:12:18 +0200

    refactor : add text in constants

---
+++ src/constants/text.ts
@@ -0,0 +1,154 @@
+export const title = "GoGreen";
+
+export const buttons = { main: "join us", more: "Read more" };
+
+export const contact = "Contact us";
+
+export const pages = [
+  {
+    title: "Home",
+    route: "home",
+  },
+  {
+    title: "About",
+    route: "about",
+  },
+  {
+    title: "Services",
+    route: "services",
+  },
+  {
+    title: "Blog",
+    route: "blog",
+  },
+  {
+    title: "Contact",
+    route: "contact",
+  },
+];
+
+export const home = {
+  title: { firstWord: "Let`s", lastWord: "together" },
+  description: "let's build a green life on our earth together with us.",
+};
+
+export const about = {
+  banner: {
+    title: "Let`s Join With Us",
+    description:
+      "by doing it together we can definitely change the world to be more natural",
+  },
+  abstracts: [
+    {
+      title: "People Working with us since 1975",
+      description:
+        "let's together rebuild our world to be better, let's make this earth green with various trees and plants that are around us for the sake of this. So that for the sake of this success, start with small things first, let's plant together, reforest, reforest and so on for comfort. our earth let's protect our earth",
+    },
+    {
+      title: "protect and take care of the earth",
+      description:
+        "let's together rebuild our world to be better, let's make this earth green with various trees and plants that are around us for the sake of this. So that for the sake of this success, start with small things first, let's plant together, reforest, reforest and so on for comfort. our earth let's protect our earth",
+    },
+    {
+      title: "reforestation, replanting, reforestation",
+      description:
+        "Reforestation can be defined as the process of replanting trees in areas that have been affected by natural disturbances like wildfires, drought, and insect and disease infestations — and unnatural ones like logging, mining, agricultural clearing, and development.",
+    },
+  ],
+};
+
+export const services = {
+  firstSection: {
+    title: { green: "5000+ ", black: "WORKING WITH US" },
+    subtitle: "What We Are Doing",
+    content: [
+      {
+        title: "use of wind  ",
+        description:
+          "Wind direction and velocity have significant influence on crop growth. ",
+      },
+      {
+        title: "Eco System",
+        description:
+          "They maintain the physical and chemical integrity of those ecosystems. ",
+      },
+      {
+        title: "Protecting",
+        description:
+          "Forest refining means the complete elimination of lianes, weed and defective trees, shrubs and herbs, subject to silvicultural desirability",
+      },
+      {
+        title: "Water Refinning  ",
+        description:
+          " filtering sediments and other pollutants from the water in the soil before it reaches a water source, such as a stream, lake or river. ",
+      },
+      {
+        title: "Recycling",
+        description:
+          "As recycling saves energy it also reduces greenhouse gas emissions, which helps to tackle climate change.",
+      },
+      { title: "Save Animal", description: "protect animals from extinction " },
+    ],
+  },
+  secondSection: {
+    title: "Testimonials",
+    subtitle: "",
+    banner: {
+      name: "Olivia Slive",
+      about:
+        "Hi my name is Olivia Slive I am a member here and I have been participating in this activity for about a year and well it not only restores the world well but is also very fun in every process that goes through.Come join GoGreen with us",
+    },
+  },
+};
+
+export const blog = {
+  posts: [
+    {
+      title: "Together we can definitely change it",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+      person: "Mira",
+      commentsNumber: "3",
+    },
+    {
+      title: "Together we can for forest",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+      person: "Alice",
+      commentsNumber: "2",
+    },
+    {
+      title: "Natural`s World",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+      person: "Chris",
+      commentsNumber: "7",
+    },
+
+    {
+      description: "protect animals from extinction ",
+    },
+  ],
+  flickPhotos: { title: "Flick Photos" },
+  recentPost: {
+    title: "Recent posts",
+    posts: [
+      { title: "Natural With Forest  ", date: "July,20,2022" },
+      { title: "Natural With World", date: "July,15,2022" },
+      { title: "The Refinning World`s", date: "July,6,2022" },
+    ],
+  },
+  tags: {
+    title: "Tags",
+    tags: ["Ecosystem", "Organic", "Solar Energy", "Wind Energy", "Plants"],
+  },
+};
+
+export const team = [
+  { name: "Wade Warren", position: "Admin" },
+  { name: "Soham", position: "Admin" },
+  { name: "Tanya", position: "Admin" },
+  { name: "Kristin", position: "Admin" },
+  { name: "Gregory", position: "Admin" },
+  { name: "Darlene", position: "" },
+];
